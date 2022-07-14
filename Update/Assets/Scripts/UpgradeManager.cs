f5ff47bae2cc624586e3afb6b7ad549306eda508
Daniel Müller <daniel.daniel@gmx.ch>
2022-07-14 21:11:50 +0200

    Update UpgradeManager.cs

--- Update/Assets/Scripts/UpgradeManager.cs
+++ Update/Assets/Scripts/UpgradeManager.cs
@@ -4,45 +4,72 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
+using TMPro;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
+using Debug = System.Diagnostics.Debug;
 
 public class UpgradeManager : MonoBehaviour
 {
-    const string PATH = "https://raw.githubusercontent.com/tactivesport/update/main/";
-    readonly string CURFOLD = Environment.CurrentDirectory + @"/UpdateFolder/";
+    [SerializeField] TMP_Text _screenDebug;
     
+    readonly string URIV1 = "https://raw.githubusercontent.com/tactivesport/update/main/VersionV1.zip";
+    readonly string URIV2 = "https://raw.githubusercontent.com/tactivesport/update/main/VersionV2.zip";
+    readonly string CURFOLDER = Environment.CurrentDirectory + @"/UpdateFolder/";
+    readonly string TEMPPATH = Path.Combine(Path.GetTempPath(), "Update.zip");
+
     public void Upgrade()
     {
-        if (!Directory.Exists(CURFOLD))
+        Log("Downloading version 2");
+        Download(URIV2, "V2/V2.exe");
+    }
+
+    public void Downgrade()
+    {
+        Log("Downloading version 1");
+        Download(URIV1, "V1/V1.exe");
+    }
+
+    void Download(string uri, string executable)
+    {
+        _cacheExecutable = executable;
+        if (Directory.Exists(CURFOLDER))
         {
-            Debug.Log("Creating UpdateFolder");
-            Directory.CreateDirectory(CURFOLD);
+            Directory.Delete(CURFOLDER, true);
+            Directory.CreateDirectory(CURFOLDER);
         }
 
-        Debug.Log("Downloading V2");
-        using (var client = new WebClient())
-        {
-            client.DownloadFile(PATH + "VersionV2.zip", CURFOLD + "VersionV2.zip");
-            client.DownloadFileCompleted += ProceedUnzip;
-        }
+        var client = new WebClient();
+        client.DownloadProgressChanged += (sender, args) => Log($"Download progress: {args.ProgressPercentage}%");
+        client.DownloadFileCompleted += ProceedUnzip;
+        client.DownloadFileAsync(new Uri(uri), TEMPPATH);
     }
 
     void ProceedUnzip(object sender, AsyncCompletedEventArgs e)
     {
         if (e.Cancelled)
         {
-            Debug.Log("Download error!");
+            Log("Download cancelled error");
             return;
         }
 
-        Debug.Log("Unzipping file.");
-        ZipFile.ExtractToDirectory(CURFOLD + "VersionV2.zip", CURFOLD);
+        Log("Download completed, attempt unzipping after wiping target folder");
         
-        Debug.Log("Starting V2.");
-        Process.Start(CURFOLD + "VersionV2/V2.exe");
-        
-        Debug.Log("Quitting V1.");
+        ZipFile.ExtractToDirectory(TEMPPATH, CURFOLDER);
+
+        Log("Starting");
+        Process.Start(CURFOLDER + _cacheExecutable);
+
+        Log("Quitting");
         Application.Quit();
+        
+        // Powershell? https://stackoverflow.com/questions/58318026/how-to-uninstall-a-uwp-application-programmatically-without-admin-access-c-sharp
     }
+
+    void Log(string text)
+    {
+        UnityEngine.Debug.Log(text);
+        _screenDebug.text = text;
+    }
+
+    string _cacheExecutable;
 }
